89f1368462072d926e897c7b7bea3e1bb8e6492d
sehta7@gmail.com <sehta7@gmail.com>
2018-10-09 21:27:09 +0200

    add class Camera for positioning the view of cube

---
+++ Math3D/Camera.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math3D
+{
+    class Camera
+    {
+        public Vector3D position;
+    }
+}
