b2cd7b919fd6a3629ad9b65aa10b5160e16c06eb
sehta7@gmail.com <sehta7@gmail.com>
2018-10-09 19:00:43 +0200

    add class Side with 4 tops in 2 dimensions and in 3 dimensions

---
+++ Math3D/Side.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math3D
+{
+    class Side
+    {
+        public Vector3D[] tops3d;
+        public PointF[] tops2d;
+
+        public Side(Vector3D[] tops3d)
+        {
+            this.tops3d = tops3d;
+        }
+
+        public Side(PointF[] tops2d)
+        {
+            this.tops2d = tops2d;
+        }
+
+        public Side(Vector3D[] tops3d, PointF[] tops2d)
+        {
+            this.tops3d = tops3d;
+            this.tops2d = tops2d;
+        }
+    }
+}
