7466de668dc5b0d1eb0169b53cf4d8567d55f763
sehta7@gmail.com <sehta7@gmail.com>
2018-10-10 12:31:24 +0200

    change order of tops to drawing

--- Math3D/Cube.cs
+++ Math3D/Cube.cs
@@ -27,59 +27,60 @@ namespace Math3D
             Vector3D[] front = new Vector3D[4];
             front[0] = new Vector3D(width, height, 0);
             front[1] = new Vector3D(0, height, 0);
-            front[2] = new Vector3D(width, 0, 0);
-            front[3] = new Vector3D(0, 0, 0);
+            front[2] = new Vector3D(0, 0, 0);
+            front[3] = new Vector3D(width, 0, 0);
             this.front = new Side(front);
 
             //back
             Vector3D[] back = new Vector3D[4];
             back[0] = new Vector3D(width, height, depth);
             back[1] = new Vector3D(0, height, depth);
-            back[2] = new Vector3D(width, 0, depth);
-            back[3] = new Vector3D(0, 0, depth);
+            back[2] = new Vector3D(0, 0, depth);
+            back[3] = new Vector3D(width, 0, depth);
             this.back = new Side(back);
 
             //left
             Vector3D[] left = new Vector3D[4];
             left[0] = new Vector3D(0, height, depth);
             left[1] = new Vector3D(0, 0, depth);
-            left[2] = new Vector3D(0, height, 0);
-            left[3] = new Vector3D(0, 0, 0);
+            left[2] = new Vector3D(0, 0, 0);
+            left[3] = new Vector3D(0, height, 0);
             this.left = new Side(left);
 
             //right
             Vector3D[] right = new Vector3D[4];
             right[0] = new Vector3D(width, height, 0);
             right[1] = new Vector3D(width, 0, 0);
-            right[2] = new Vector3D(width, height, depth);
-            right[3] = new Vector3D(width, 0, depth);
+            right[2] = new Vector3D(width, 0, depth);
+            right[3] = new Vector3D(width, height, depth);
             this.right = new Side(right);
 
             //top
             Vector3D[] top = new Vector3D[4];
             top[0] = new Vector3D(width, height, depth);
             top[1] = new Vector3D(0, height, depth);
-            top[2] = new Vector3D(width, height, 0);
-            top[3] = new Vector3D(0, height, 0);
+            top[2] = new Vector3D(0, height, 0);
+            top[3] = new Vector3D(width, height, 0);
             this.top = new Side(top);
 
             //bottom
             Vector3D[] bottom = new Vector3D[4];
             bottom[0] = new Vector3D(width, 0, depth);
             bottom[1] = new Vector3D(0, 0, depth);
-            bottom[2] = new Vector3D(width, 0, 0);
-            bottom[3] = new Vector3D(0, 0, 0);
+            bottom[2] = new Vector3D(0, 0, 0);
+            bottom[3] = new Vector3D(width, 0, 0);
             this.bottom = new Side(bottom);
         }
 
         public void count2D(Camera camera, float zoom, PointF drawCenter)
         {
+
             //fornt
             for (int i = 0; i < 4; i++)
             {
                 float zValue = - front.tops3d[i].z - camera.position.z;
                 front.tops2d[i].X = ((camera.position.x - front.tops3d[i].x) / zValue * zoom) + drawCenter.X;
-                front.tops2d[i].Y = ((camera.position.y - front.tops3d[i].y) / zValue * zoom) + drawCenter.Y;
+                front.tops2d[i].Y = ((camera.position.y - front.tops3d[i].y) / zValue * zoom) + drawCenter.Y;              
             }
 
             //back
