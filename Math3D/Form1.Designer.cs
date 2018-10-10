e8afb59afcf2e2ad51dea97a857a1f1eeff975a7
sehta7@gmail.com <sehta7@gmail.com>
2018-10-10 16:02:17 +0200

    add buttons to rotate cube

--- Math3D/Form1.Designer.cs
+++ Math3D/Form1.Designer.cs
@@ -36,11 +36,16 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
+            this.pictureBox1.BackColor = System.Drawing.Color.WhiteSmoke;
             this.pictureBox1.Location = new System.Drawing.Point(36, 32);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(305, 275);
@@ -106,11 +111,55 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(405, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Rotation";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(395, 167);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(18, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "x";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(419, 167);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(18, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "y";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(443, 167);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(18, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "z";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
             this.ClientSize = new System.Drawing.Size(531, 363);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -137,6 +186,10 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- Math3D/Form1.cs
+++ Math3D/Form1.cs
@@ -15,12 +15,14 @@ namespace Math3D
         Graphics graphics;
         Bitmap bitmap;
         Cube cube;
+        float zoom;
 
         public Form1()
         {
             InitializeComponent();
             bitmap = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
             graphics = Graphics.FromImage(bitmap);
+            zoom = (float)Screen.PrimaryScreen.Bounds.Width / 1.5f;
         }
 
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
@@ -40,14 +42,68 @@ namespace Math3D
         private void button1_Click(object sender, EventArgs e)
         {
             pictureBox1.Image = null;
-            graphics.Clear(Color.WhiteSmoke);
-            pictureBox1.Image = bitmap;
-            pictureBox1.Refresh();
-
-            float zoom = (float)Screen.PrimaryScreen.Bounds.Width / 1.5f;
+            bitmap = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
+            graphics = Graphics.FromImage(bitmap);
 
             cube = new Cube();
             cube.InitializeCube(Int32.Parse(textBox1.Text), Int32.Parse(textBox2.Text), Int32.Parse(textBox3.Text));
+            Camera camera = new Camera(cube.center.x, cube.center.z, ((cube.center.x * zoom) / cube.center.x));
+
+            cube.count2D(camera, zoom, new PointF(pictureBox1.Size.Width / 2, pictureBox1.Size.Height / 2));
+
+            bitmap = Drawing.drawingCube(cube, graphics, bitmap);
+            pictureBox1.Image = bitmap;
+            pictureBox1.Refresh();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            bitmap = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
+            graphics = Graphics.FromImage(bitmap);
+
+            cube = new Cube();
+            cube.InitializeCube(100, 100, 100);
+            cube.xRotation(25);
+
+            Camera camera = new Camera(cube.center.x, cube.center.z, ((cube.center.x * zoom) / cube.center.x));
+
+            cube.count2D(camera, zoom, new PointF(pictureBox1.Size.Width / 2, pictureBox1.Size.Height / 2));
+
+            bitmap = Drawing.drawingCube(cube, graphics, bitmap);
+            pictureBox1.Image = bitmap;
+            pictureBox1.Refresh();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            bitmap = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
+            graphics = Graphics.FromImage(bitmap);
+
+            cube = new Cube();
+            cube.InitializeCube(100, 100, 100);
+            cube.yRotation(25);
+
+            Camera camera = new Camera(cube.center.x, cube.center.z, ((cube.center.x * zoom) / cube.center.x));
+
+            cube.count2D(camera, zoom, new PointF(pictureBox1.Size.Width / 2, pictureBox1.Size.Height / 2));
+
+            bitmap = Drawing.drawingCube(cube, graphics, bitmap);
+            pictureBox1.Image = bitmap;
+            pictureBox1.Refresh();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            bitmap = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
+            graphics = Graphics.FromImage(bitmap);
+
+            cube = new Cube();
+            cube.InitializeCube(100, 100, 100);
+            cube.zRotation(25);
+
             Camera camera = new Camera(cube.center.x, cube.center.z, ((cube.center.x * zoom) / cube.center.x));
 
             cube.count2D(camera, zoom, new PointF(pictureBox1.Size.Width / 2, pictureBox1.Size.Height / 2));
