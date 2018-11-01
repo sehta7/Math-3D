f8e1be40d970478738b2488d899b22fe8286180d
sehta7@gmail.com <sehta7@gmail.com>
2018-11-01 14:57:00 +0100

    fix drawing of cube

--- Math3D/Form1.cs
+++ Math3D/Form1.cs
@@ -22,21 +22,24 @@ namespace Math3D
             InitializeComponent();
             bitmap = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
             graphics = Graphics.FromImage(bitmap);
-            zoom = (float)Screen.PrimaryScreen.Bounds.Width / 1.5f;
         }
 
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
-            float zoom = (float)Screen.PrimaryScreen.Bounds.Width / 1.5f;
+            zoom = (float)Screen.PrimaryScreen.Bounds.Width / 1.5f;
 
-            Cube cubeInit = new Cube();
-            cubeInit.InitializeCube(100, 100, 100);
-            Camera camera = new Camera(cubeInit.center.x, cubeInit.center.z, ((cubeInit.center.x * zoom) / cubeInit.center.x));
+            if (cube == null)
+            {
+                cube = new Cube();
+                cube.InitializeCube(100, 100, 100);
+                Camera camera = new Camera(cube.center.x, cube.center.z, ((cube.center.x * zoom) / cube.center.x));
 
-            cubeInit.count2D(camera, zoom, new PointF(pictureBox1.Size.Width / 2, pictureBox1.Size.Height / 2));
+                cube.count2D(camera, zoom, new PointF(pictureBox1.Size.Width / 2, pictureBox1.Size.Height / 2));
 
-            pictureBox1.Image = Drawing.drawingCube(cubeInit, graphics, bitmap);
-            pictureBox1.Refresh();
+                pictureBox1.Image = Drawing.drawingCube(cube, graphics, bitmap);
+                //pictureBox1.Refresh();
+            }
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -44,6 +47,8 @@ namespace Math3D
             pictureBox1.Image = null;
             bitmap = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
             graphics = Graphics.FromImage(bitmap);
+
+            zoom = (float)Screen.PrimaryScreen.Bounds.Width / 1.5f;
 
             cube = new Cube();
             cube.InitializeCube(Int32.Parse(textBox1.Text), Int32.Parse(textBox2.Text), Int32.Parse(textBox3.Text));
@@ -62,8 +67,14 @@ namespace Math3D
             bitmap = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
             graphics = Graphics.FromImage(bitmap);
 
-            cube = new Cube();
-            cube.InitializeCube(100, 100, 100);
+            zoom = (float)Screen.PrimaryScreen.Bounds.Width / 1.5f;
+
+            if (cube == null)
+            {
+                cube = new Cube();
+                cube.InitializeCube(100, 100, 100);
+            }
+            
             cube.xRotation(25);
 
             Camera camera = new Camera(cube.center.x, cube.center.z, ((cube.center.x * zoom) / cube.center.x));
@@ -81,8 +92,13 @@ namespace Math3D
             bitmap = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
             graphics = Graphics.FromImage(bitmap);
 
-            cube = new Cube();
-            cube.InitializeCube(100, 100, 100);
+            zoom = (float)Screen.PrimaryScreen.Bounds.Width / 1.5f;
+
+            if (cube == null)
+            {
+                cube = new Cube();
+                cube.InitializeCube(100, 100, 100);
+            }
             cube.yRotation(25);
 
             Camera camera = new Camera(cube.center.x, cube.center.z, ((cube.center.x * zoom) / cube.center.x));
@@ -100,8 +116,13 @@ namespace Math3D
             bitmap = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
             graphics = Graphics.FromImage(bitmap);
 
-            cube = new Cube();
-            cube.InitializeCube(100, 100, 100);
+            zoom = (float)Screen.PrimaryScreen.Bounds.Width / 1.5f;
+
+            if (cube == null)
+            {
+                cube = new Cube();
+                cube.InitializeCube(100, 100, 100);
+            }
             cube.zRotation(25);
 
             Camera camera = new Camera(cube.center.x, cube.center.z, ((cube.center.x * zoom) / cube.center.x));
